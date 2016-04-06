eac2071c7aeb62b5dcdd0773984dc3b0429f77b9
Vasileios Giannokostas <giannokostas@hotmail.com>
2016-04-06 17:26:34 +0200

    App Resources complete except RemoveAll

--- src/main/java/io/hops/metadata/yarn/dal/YarnApplicationResourcesDataAccess.java
+++ src/main/java/io/hops/metadata/yarn/dal/YarnApplicationResourcesDataAccess.java
@@ -2,8 +2,19 @@ package io.hops.metadata.yarn.dal;
 
 import io.hops.exception.StorageException;
 import io.hops.metadata.common.EntityDataAccess;
+import io.hops.metadata.yarn.entity.YarnApplicationResources;
+
+import java.util.Collection;
+import java.util.Map;
 
 public interface YarnApplicationResourcesDataAccess<T> extends EntityDataAccess {
 
     void add(T YarnApplicationResources) throws StorageException;
+
+    void removeAll(Collection<YarnApplicationResources> killed)
+            throws StorageException;
+
+    YarnApplicationResources findById(int inodeID) throws StorageException;
+
+    Map<Integer, T> getAll() throws StorageException;
 }
